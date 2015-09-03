61f2b49ab4fc9df06fda9f05666f995a796a6925
Robert Blair Mason Jr. <rbmj@verizon.net>
2015-09-03 13:45:07 -0400

    Clean up domain joining script
    Don't need ntp anymore - just use the systemd thing (it's easy to conf, anyway).
    
    Also, some small tweaks to makethings cleaner.

--- join.sh
+++ join.sh
@@ -2,11 +2,11 @@
 set -e
 
 CACHE=0
-if [ $# -ne 4 ] ; then
-	if [ $# -eq 5 ] && [ $5 = 'CACHE' ] ; then
+if [ $# -ne 3 ] ; then
+	if [ $# -eq 4 ] && [ $4 = 'CACHE' ] ; then
 		CACHE=1
 	else
-		echo "USAGE: $0 hostname domain dchostname username ['CACHE']" >&2
+		echo "USAGE: $0 hostname domain username ['CACHE']" >&2
 		echo "Example: $0 iwg-gen0 d3catur.net dc0 rbmason" >&2
 		exit 1
 	fi
@@ -14,8 +14,7 @@ fi
 
 HOST=$1
 DOMAIN=$2
-DCHOSTNAME=$3
-USER=$4
+USER=$3
 
 set -x
 
@@ -23,43 +22,32 @@ hostname $HOST
 echo "127.0.0.1    $HOST.$DOMAIN      $HOST      localhost" > /etc/hosts
 echo "$HOST" > /etc/hostname
 
-apt-get install ntp realmd adcli sssd libsss-sudo crudini -y
+apt-get install realmd adcli sssd libsss-sudo crudini -y
 mkdir -p /var/lib/samba/private
 systemctl enable sssd
 
-cat > /etc/ntp.conf << EOF
-driftfile /var/lib/ntp/ntp.drift
-statistics loopstats peerstats clockstats
-filegen loopstats file loopstats type day enable
-filegen peerstats file peerstats type day enable
-filegen clockstats file clockstats type day enable
+# Get Domain Controller
+DCURL=`host -t SRV _ldap._tcp.dc._msdcs.$DOMAIN | cut '-d ' -f8 | sed 's/.$//'`
 
-# Put your ntp server here
-# It might not be the domain controller - they don't have to
-# be the same computer (might be e.g. ntp.usna.bluenet)
-server $DCHOSTNAME.$DOMAIN
-
-restrict -4 default kod notrap nomodify nopeer noquery
-restrict -6 default kod notrap nomodify nopeer noquery
-restrict 127.0.0.1
-restrict ::1
-EOF
-
-service ntp stop
-ntpd -gq
-service ntp start
+crudini --set /etc/systemd/timesyncd.conf Time Servers $DCURL
+timedatectl set-ntp true
+# Let time sync
+sleep 5
 
 realm discover $DOMAIN
 realm join --user=$USER $DOMAIN
 crudini --set /etc/sssd/sssd.conf sssd default_domain_suffix $DOMAIN
 systemctl start sssd
 
-echo 'session required pam_mkhomedir.so' >> /etc/pam.d/common-session
-echo "%domain\ admins@$DOMAIN ALL=(root) ALL" >> /etc/sudoers
+# If not found in common-session, add pam_mkhomedir.so to pam common-session so
+# domain users will have home directories.
+grep pam_mkhomedirs.so /etc/pam.d/common-session > /dev/null && \
+    echo 'session required pam_mkhomedir.so' >> /etc/pam.d/common-session
+echo "%domain\ admins@$DOMAIN ALL=(root) ALL" >> /etc/sudoers.d/admins_$DOMAIN
 
 [ $CACHE -eq 1 ] && apt-get install nss-updatedb libnss-db libpam-ccreds nscd -y
 
 set +x
 
-echo SUCCESS
+echo SUCCESS - you must restart in order to complete the join.
 
