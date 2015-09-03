04b9558434b502accccfcd29670222bd401be842
Robert Blair Mason Jr. <rbmj@verizon.net>
2015-09-03 12:09:28 -0400

    Simplify join script by using realmd and sssd
    Winbind/samba is disgusting.  This is the more modern solution.  Tested/works on debian >= 8.0.
    
    Winbind doesn't even keep IDs mapped properly, so this provides a lot less opportunities to fuck up the configuration.

--- join.sh
+++ join.sh
@@ -14,8 +14,6 @@ fi
 
 HOST=$1
 DOMAIN=$2
-DOMAIN_UPPER=${DOMAIN^^}
-WINS=`echo $DOMAIN_UPPER | cut -d. -f1`
 DCHOSTNAME=$3
 USER=$4
 
@@ -25,7 +23,9 @@ hostname $HOST
 echo "127.0.0.1    $HOST.$DOMAIN      $HOST      localhost" > /etc/hosts
 echo "$HOST" > /etc/hostname
 
-apt-get install krb5-user libpam-krb5 winbind libpam-winbind ntp libnss-winbind -y
+apt-get install ntp realmd adcli sssd libsss-sudo crudini -y
+mkdir -p /var/lib/samba/private
+systemctl enable sssd
 
 cat > /etc/ntp.conf << EOF
 driftfile /var/lib/ntp/ntp.drift
@@ -49,81 +49,17 @@ service ntp stop
 ntpd -gq
 service ntp start
 
-cat > /etc/krb5.conf << EOF
-[logging]
-        default = FILE:/var/log/krb5libs.log
-        kdc = FILE:/var/log/krb5kdc.log
-        admin_server = FILE:/var/log/kadmind.log
-
-[libdefaults]
-        default_realm = $DOMAIN_UPPER
-
-[realms]
-        $DOMAIN_UPPER = {
-                kdc = $DCHOSTNAME.$DOMAIN
-                admin_server = $DCHOSTNAME.$DOMAIN
-        }
-
-[domain_realm]
-        .$DOMAIN = $DOMAIN_UPPER
-        $DOMAIN = $DOMAIN_UPPER
-EOF
-
-kinit $USER@$DOMAIN_UPPER
-
-cat > /etc/samba/smb.conf << EOF
-[global]
-        security = ads
-        realm = $DOMAIN_UPPER
-        workgroup = $WINS
-        # winbind needs this but we wont' use this
-        idmap config * : backend = tdb
-        idmap config * : range = 90000-99999
-        # don't collide with system accounts
-        idmap config $WINS : backend = rid
-        idmap config $WINS : range = 100000-200000
-        idmap config $WINS : base_rid = 0
-        winbind enum users = yes
-        winbind enum groups = yes
-        client use spnego = yes
-        client ntlmv2 auth = yes
-        client ldap sasl wrapping = sign
-        encrypt passwords = yes
-        winbind use default domain = yes
-        # needed to ensure users have a login shell
-        template shell = /bin/bash
-        template homedir = /home/%U
-        # Don't allow anonymous connections
-        restrict anonymous = 2
-EOF
-
-service winbind stop
-service winbind start
-net ads join -U $USER -W $DOMAIN_UPPER
+realm discover $DOMAIN
+realm join --user=$USER $DOMAIN
+crudini --set /etc/sssd/sssd.conf sssd default_domain_suffix $DOMAIN
+systemctl start sssd
 
 echo 'session required pam_mkhomedir.so' >> /etc/pam.d/common-session
-
-cat > /etc/nsswitch.conf << EOF
-passwd:     compat winbind
-group:      compat winbind
-shadow:     compat
-
-hosts:      files mdns4_minimal [NOTFOUND=return] dns mdns4
-networks:   files
-
-protocols:  db files
-services:   db files
-ethers:     db files
-rpc:        db files
-
-netgroup:   nis
-EOF
-
-echo '%domain\ admins ALL=(root) ALL' >> /etc/sudoers
-service winbind restart
+echo "%domain\ admins@$DOMAIN ALL=(root) ALL" >> /etc/sudoers
 
 [ $CACHE -eq 1 ] && apt-get install nss-updatedb libnss-db libpam-ccreds nscd -y
 
 set +x
 
 echo SUCCESS
+
